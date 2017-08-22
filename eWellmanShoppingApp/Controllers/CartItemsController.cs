6231dc9c2baa973cf5680944f4e5a4c3d6e8bb53
EricJW <Eric.J.Wellman@gmail.com>
2017-08-22 16:58:34 -0400

    aug22 17 update

--- eWellmanShoppingApp/Controllers/CartItemsController.cs
+++ eWellmanShoppingApp/Controllers/CartItemsController.cs
@@ -14,47 +14,47 @@ using Microsoft.Ajax.Utilities;
 
 namespace eWellmanShoppingApp.Controllers
 {
-    public class CartItemsController : Universal{
+	public class CartItemsController : Universal{
 
-        // GET: CartItems
-        public ActionResult Index()
-        {
-            return View(db.cartItems.ToList());
+		// GET: CartItems
+		public ActionResult Index()
+		{
+			return View(db.cartItems.ToList());
 		}
 
 		
 
-        // GET: CartItems/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CartItem cartItem = db.cartItems.Find(id);
-            if (cartItem == null)
-            {
-                return HttpNotFound();
-            }
+		// GET: CartItems/Details/5
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			CartItem cartItem = db.cartItems.Find(id);
+			if (cartItem == null)
+			{
+				return HttpNotFound();
+			}
 			//foreach (var row in db.cartItems.SqlQuery("SELECT * FROM CartItems WHERE customerID=@0")) {
 
 			//}
 
 			return View(cartItem);
-        }
+		}
 
-        //// GET: CartItems/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
+		//// GET: CartItems/Create
+		//public ActionResult Create()
+		//{
+		//    return View();
+		//}
 
-        // POST: CartItems/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(int? idIn) {
+		// POST: CartItems/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(int? idIn) {
 			if (idIn == null) {
 				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 			}
@@ -69,76 +69,86 @@ namespace eWellmanShoppingApp.Controllers
 				customerID = user.Id,
 				itemID = itemIn.id
 			};
-			db.cartItems.Add(cartItem);
-			db.SaveChanges();
+			CartItem itemToCheck = db.cartItems.SingleOrDefault(item => item.itemID == cartItem.itemID && item.customerID == cartItem.customerID);
+			if (itemToCheck == null) {
+				db.cartItems.Add(cartItem);
+				db.SaveChanges();
+			}
+			else if (itemToCheck != null) {
+				int index = db.cartItems.Find(itemToCheck.id).id;
+				db.cartItems.Find(index).count++;
+				db.SaveChanges();
+			}
 
 			return Redirect(HttpContext.Request.UrlReferrer.AbsoluteUri);
 		}
 
-		// GET: CartItems/Edit/5
-		public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CartItem cartItem = db.cartItems.Find(id);
-            if (cartItem == null)
-            {
-                return HttpNotFound();
-            }
-            return View(cartItem);
-        }
+		//// GET: CartItems/Edit/5
+		//public ActionResult Edit(int? id)
+  //      {
+  //          if (id == null)
+  //          {
+  //              return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+  //          }
+  //          CartItem cartItem = db.cartItems.Find(id);
+  //          if (cartItem == null)
+  //          {
+  //              return HttpNotFound();
+  //          }
+  //          return View(cartItem);
+  //      }
 
-        // POST: CartItems/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,customerID,itemID,count,creationDate")] CartItem cartItem)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(cartItem).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(cartItem);
-        }
+		// POST: CartItems/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int? idIn, int countIn)
+		{
+			db.cartItems.Find(idIn).count = countIn;
+			db.SaveChanges();
+			//Original code
+			//if (ModelState.IsValid) {
+			//	db.Entry(cartItem).State = EntityState.Modified;
+			//	db.SaveChanges();
+			//	return RedirectToAction("Index");
+			//}
+			return Redirect(HttpContext.Request.UrlReferrer.AbsoluteUri);
+		}
 
-        // GET: CartItems/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            CartItem cartItem = db.cartItems.Find(id);
-            if (cartItem == null)
-            {
-                return HttpNotFound();
-            }
-            return View(cartItem);
-        }
+		// GET: CartItems/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			CartItem cartItem = db.cartItems.Find(id);
+			if (cartItem == null)
+			{
+				return HttpNotFound();
+			}
+			return View(cartItem);
+		}
 
-        // POST: CartItems/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            CartItem cartItem = db.cartItems.Find(id);
-            db.cartItems.Remove(cartItem);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+		// POST: CartItems/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			CartItem cartItem = db.cartItems.Find(id);
+			db.cartItems.Remove(cartItem);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
 }
